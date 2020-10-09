de364fb8a112806094864e5b84b7603af58ccdc1
Amos Chihi <appschihi@gmail.com>
2020-10-09 23:32:00 +0300

    Charts & Components

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 
 @Component({
   selector: 'app-countries',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+  data: GlobalDataSummary[] = [];
+  countries: string[] = [];
 
-  constructor() { }
+  totalConfirmed = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  totalActive = 0;
+
+  constructor(private dataService: DataServiceService) { }
 
   ngOnInit(): void {
+    this.dataService.getGlobalData().subscribe(result=>{
+      this.data = result;
+      this.data.forEach(cs => {
+        this.countries.push(cs.country);
+      })
+    })
+  }
+
+  updateValues(country: string){
+    this.data.forEach(cs=>{
+      if(cs.country == country) {
+        this.totalActive = cs.active;
+        this.totalRecovered = cs.recovered;
+        this.totalDeaths = cs.deaths;
+        this.totalConfirmed = cs.confirmed;
+      }
+    })
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/services/data-service.service';
 import { GlobalDataSummary } from 'src/app/models/global-data';
+import { GoogleChartInterface } from 'ng2-google-charts';
 
 
 @Component({
@@ -14,8 +15,50 @@ export class HomeComponent implements OnInit {
   totalRecovered = 0;
   totalActive = 0;
   globalData: GlobalDataSummary[];
+  piechart: GoogleChartInterface = {
+    chartType: 'PieChart'
+  };
+  columnchart: GoogleChartInterface = {
+    chartType: 'ColumnChart'
+  }
 
   constructor(private dataService: DataServiceService) { }
+  
+  initChart(caseType: string) {
+    let datatable = [];
+    datatable.push(['Country', 'Cases'])
+    this.globalData.forEach(cs=>{
+      let value: number;
+      if(caseType=='c')
+        value = cs.confirmed;
+      if(caseType=='r')
+        value = cs.recovered;
+      if(caseType=='d')
+        value  = cs.deaths;
+      if(caseType == 'a')
+        value =cs.active;
+      
+      datatable.push([cs.country, value])
+    })
+
+    console.log(datatable);
+    this.piechart = {
+      chartType: 'PieChart',
+      dataTable: datatable,
+      //firstRowIsData: true,
+      options: {
+        height: 500
+      },
+    };
+    this.columnchart = {
+      chartType: 'ColumnChart',
+      dataTable: datatable,
+      //firstRowIsData: true,
+      options: {
+        height: 500
+      },
+    };
+  }
 
   ngOnInit(): void {
     this.dataService.getGlobalData()
@@ -31,10 +74,13 @@ export class HomeComponent implements OnInit {
               this.totalActive+=cs.active
             }
           })
+          this.initChart('c');
         }
-      }
-    )
+      })
   }
 
+  updateChart(input: HTMLInputElement){
+    this.initChart(input.value);
+  }
   
 }
